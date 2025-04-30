1b9d38a6aa7d0ae8f7c1f487788ef88ac673a2e8
HuyDNA <huydo862003@gmail.com>
2025-04-30 11:04:09 +0700

    chore: add sound and graphic lib

--- flake.nix
+++ flake.nix
@@ -16,11 +16,29 @@
           python311
           python311Packages.numpy
           neovim
+          xorg.libX11
+          xorg.libXext
+          xorg.libXrender
+          xorg.libXcomposite
+          xorg.libXfixes
+          xorg.libXdamage
+          mesa
+          libGL
+          openal
+          alsa-lib
+          libpulseaudio
         ];
         shellHook = ''
           uv sync
         '';
-        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
+          pkgs.libGL
+          pkgs.xorg.libX11
+          pkgs.mesa
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libpulseaudio
+          pkgs.alsa-lib
+        ]}" ;
       };
     });
 }
